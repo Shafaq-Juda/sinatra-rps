2eb1d487c6b78f5886123551290c4bfe039bf81a
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2024-01-08 06:56:17 +0000

    Successfully refactored the code and added a prompt for invalid input.

--- app.rb
+++ app.rb
@@ -2,8 +2,31 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:homepage)
+end
+
+get("/:play") do
+ @our_move = params.fetch("play")
+
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  
+  if @comp_move == @our_move
+    @outcome = "tied"
+  elsif @comp_move == "rock" && @our_move == "paper"
+    @outcome = "won"
+  elsif @comp_move == "rock" && @our_move == "scissors"
+    @outcome = "lost"
+  elsif @comp_move == "paper" && @our_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper" && @our_move == "scissors"
+    @outcome = "won"
+  elsif @comp_move == "scissors" && @our_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "scissors" && @our_move == "paper"
+    @outcome = "lost"
+ 
+  end  
+
+  erb(:game)
 end
